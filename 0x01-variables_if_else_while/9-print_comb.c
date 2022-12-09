399bba2ef0d55506337a71aa597d22c3158aca65
Nicktruser <Nicktrusere@gmail.com>
2022-12-09 14:36:11 -0800

    9-print_comb.c patience

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints all combinations of single digits
+ * main - print all combinations of single digits numbers
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int a;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a <= 10; a++)
 	{
 		putchar(a + '0');
 		if (a < 9)
@@ -17,6 +17,7 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
