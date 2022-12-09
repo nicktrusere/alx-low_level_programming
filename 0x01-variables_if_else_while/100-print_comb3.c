1f718b1da9a78d097d35e6e6c6e2a0260e843b7d
Nicktruser <Nicktrusere@gmail.com>
2022-12-09 13:37:40 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Print combination of two digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int tens;
+	int ones;
+
+	for (tens = 0; tens <= 9; tens++)
+	{
+		for (ones = tens + 1; ones <= 9; ones++)
+		{
+			putchar(tens + '0');
+			putchar(ones + '0');
+			if (tens < 0)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
