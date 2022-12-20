b5c216e8827a6decb9fbdb03ffb90b1b7f70aef0
Nicktruser <Nicktrusere@gmail.com>
2022-12-20 11:03:51 -0800

    3 puts c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the stdout
+ * @str: the string
+ * Return: the length of the string
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(sytr[a]);
+		a++;
+	}
+}
