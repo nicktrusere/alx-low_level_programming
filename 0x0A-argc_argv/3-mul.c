f764b61acfe96442b1a2a3eeef4af3700ac622bd
Nicktruser <Nicktrusere@gmail.com>
2023-01-09 11:16:49 -0800

    Task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	}
 	for (index = 1; index < argc; index++)
 	{
-		multiplication - multiplication * atoi(argv[index]);
+		multiplication = multiplication * atoi(argv[index]);
 	}
 	printf("%d\n", multiplication);
 	return (0);
