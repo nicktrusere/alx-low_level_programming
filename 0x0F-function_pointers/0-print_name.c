63e12048f2cff47c346ea69d6d4ca7e0268d9227
Nicktruser <Nicktrusere@gmail.com>
2023-01-18 05:45:39 -0800

    0.What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print name using pointer function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This is a 0x0F. C - Function pointers readme file.
