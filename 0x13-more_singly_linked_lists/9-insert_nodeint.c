a99c4aa2fc282dfec4bae9a33a0da39fd6437858
Nicktruser <Nicktrusere@gmail.com>
2023-01-30 11:38:58 -0800

    insert updated

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,22 +23,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = NULL;
 
 	if (idx == 0)
+	{
 		new->next = *head;
-	*head = new;
-	return (new);
-}
+		*head = new;
+		return (new);
+	}
 
-for (i = 0; temp && i < idx; i++)
-{
-	if (i == idx - 1)
-}
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
 
-new->next = temp->next;
-temp->next = new;
-return (new);
-}
-else
-temp = temp->next;
-}
-return (NULL);
+	return (NULL);
 }
